75901d8da59c736c4908129dcd2c93e13bff8cda
Anmoldeep Singh <go93net@mytum.de>
2025-11-02 23:50:46 +0100

    Day22_Complete

---
+++ Day21-Function_Arguments/main.py
@@ -0,0 +1,26 @@
+# def average(a, b, c=1):
+#   print("The average is ", (a + b + c) / 3)
+
+
+def average(*numbers):
+  # print(type(numbers))
+  sum = 0
+  for i in numbers:
+    sum = sum + i
+  # print("Average is: ", sum / len(numbers))
+  # return 7
+  return sum / len(numbers)
+
+print(average(1,2,3,4,5))
+# average(b=9)
+
+c = average(5, 6, 7, 1)
+print(c)
+
+
+# def name(**name):
+#   # print(type(name))
+#   print("Hello,", name["fname"], name["mname"], name["lname"])
+
+
+# name(mname="Buchanan", lname="Barnes", fname="James")
